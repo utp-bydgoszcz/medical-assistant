5f4e03d0d7963fba404570995d96c0d7e69bb96a
bartoszcwiklinski <barcwi002@utp.edu.pl>
2018-04-14 00:09:21 +0200

    fix

--- src/main/java/pl/edu/utp/medicalassistant/controller/GetInformationController.java
+++ src/main/java/pl/edu/utp/medicalassistant/controller/GetInformationController.java
@@ -1,7 +1,10 @@
 package pl.edu.utp.medicalassistant.controller;
 
 import io.swagger.annotations.Api;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(allowedHeaders = {"Access-Control-Allow-Origin", "*"})
 @Api(value = "Get Information Controller", description = "REST API for get information.", tags = {"Medical Assistant Application"})
 public class GetInformationController {
+
+    @GetMapping("/get-configuration")
+    public ResponseEntity getConfiguration(){
+        return new ResponseEntity("", HttpStatus.OK);
+    }
+
+    @GetMapping("/get-events")
+    public ResponseEntity getEvents(){
+        return new ResponseEntity("", HttpStatus.OK);
+    }
 }
--- src/main/java/pl/edu/utp/medicalassistant/controller/PhotoAndAEDController.java
+++ src/main/java/pl/edu/utp/medicalassistant/controller/PhotoAndAEDController.java
@@ -1,13 +1,23 @@
 package pl.edu.utp.medicalassistant.controller;
 
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/photo-aed")
 @CrossOrigin(allowedHeaders = {"Access-Control-Allow-Origin", "*"})
 @Api(value = "Photo And AED Controller", description = "REST API for photo and aed.", tags = {"Medical Assistant Application"})
 public class PhotoAndAEDController {
+
+    @GetMapping("/get-photo/{name}")
+    public ResponseEntity getPhoto(@PathVariable String name){
+        return new ResponseEntity("", HttpStatus.OK);
+    }
+
+    @GetMapping("/get-aed")
+    public ResponseEntity getAedList(){
+        return new ResponseEntity("", HttpStatus.OK);
+    }
 }
