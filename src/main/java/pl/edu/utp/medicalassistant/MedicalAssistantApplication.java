1291d883d3464ee3a201d13f1d06eaf843098439
Damian <damian.ledzinski@utp.edu.pl>
2018-04-13 23:42:24 +0200

    initialization

---
+++ src/main/java/pl/edu/utp/medicalassistant/MedicalAssistantApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.utp.medicalassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAssistantApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/edu/utp/medicalassistant/MedicalAssistantApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.edu.utp.medicalassistant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedicalAssistantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
