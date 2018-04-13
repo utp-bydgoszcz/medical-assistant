4decf3323285a578104ff396833e01ac4351b5c4
Cezary Kolaszewski <cezxx1@gmail.com>
2018-04-14 00:23:14 +0200

    Android push notifications

---
+++ src/main/java/pl/edu/utp/medicalassistant/controller/AndroidNotificationController.java
@@ -0,0 +1,63 @@
+package pl.edu.utp.medicalassistant.controller;
+
+import com.oracle.javafx.jmx.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.utp.medicalassistant.service.AndroidPushNotificationService;
+
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping(value = "/android/notification")
+public class AndroidNotificationController {
+
+    private final String TOPIC = "test";
+
+    private final AndroidPushNotificationService androidPushNotificationService;
+
+    @Autowired
+    public AndroidNotificationController(AndroidPushNotificationService androidPushNotificationService) {
+        this.androidPushNotificationService = androidPushNotificationService;
+    }
+
+    @GetMapping(value = "/send",produces = "application/json")
+    public ResponseEntity<String> send(@RequestBody HashMap<String,Object> sendData,
+                                       @RequestParam String notificationTitle,@RequestParam String notificationData) throws JSONException {
+
+        JSONObject body = new JSONObject();
+        body.put("to", "/topics/" + TOPIC);
+        body.put("priority", "high");
+
+        JSONObject notification = new JSONObject();
+        notification.put("title", notificationTitle);
+        notification.put("body", notificationData);
+
+        JSONObject data = new JSONObject();
+        data.put("data", sendData);
+
+
+        body.put("notification", notification);
+        body.put("data", data);
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString());
+
+        CompletableFuture<String> pushNotification = androidPushNotificationService.send(request);
+        CompletableFuture.allOf(pushNotification).join();
+
+        try {
+            String firebaseResponse = pushNotification.get();
+
+            return new ResponseEntity<>(firebaseResponse, HttpStatus.OK);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>("Push Notification ERROR!", HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/pl/edu/utp/medicalassistant/service/AndroidPushNotificationService.java
@@ -0,0 +1,41 @@
+package pl.edu.utp.medicalassistant.service;
+
+
+import lombok.extern.slf4j.Slf4j;
+import pl.edu.utp.medicalassistant.model.interceptors.HeaderRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@Slf4j
+public class AndroidPushNotificationService {
+
+    @Value("${firebase.server.key}")
+    private String FIREBASE_SERVER_KEY;
+
+    @Value("${firebase.api.url}")
+    private String FIREBASE_API_URL;
+
+    @Async
+    public CompletableFuture<String> send(HttpEntity<String> entity) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ArrayList<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new HeaderRequestInterceptor("Authorization", "key=" + FIREBASE_SERVER_KEY));
+        interceptors.add(new HeaderRequestInterceptor("Content-Type", "application/json"));
+        restTemplate.setInterceptors(interceptors);
+
+        String firebaseResponse = restTemplate.postForObject(FIREBASE_API_URL, entity, String.class);
+
+        return CompletableFuture.completedFuture(firebaseResponse);
+    }
+
+}
