29227103b7dcab6bd6d50da5959104d3a640a33a
Damian <damian.ledzinski@utp.edu.pl>
2018-04-14 10:19:02 +0200

    ping controller

--- src/main/java/pl/edu/utp/medicalassistant/model/UserRole.java
+++ src/main/java/pl/edu/utp/medicalassistant/model/UserRole.java
@@ -1,15 +1,41 @@
 package pl.edu.utp.medicalassistant.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+
 @Data
-public class UserRole implements GrantedAuthority{
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRole implements GrantedAuthority {
 
-    private Long id;
-    private String role;
+	private Role role;
 
-    @Override
-    public String getAuthority() {
-        return role;
-    }
+	@Override
+	public String getAuthority() {
+		return role.getId();
+	}
+
+	public static enum Role {
+		MEDICAL_RESCUER("dedical-rescuer", "Ratownik medyczny"),
+		RESCUER("rescuer", "Ratownik"),
+		PASSERBY("other-person", "Niewykwalifikowany");
+
+		@Getter
+		private final String id;
+		private final String name;
+
+		private Role(String id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+
+	}
 }
