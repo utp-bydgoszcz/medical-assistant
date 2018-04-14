306305852b380198bea86737f8782d245fc84e0c
bartoszcwiklinski <barcwi002@utp.edu.pl>
2018-04-14 09:56:41 +0200

    add models

---
+++ src/main/java/pl/edu/utp/medicalassistant/model/Rescuer.java
@@ -0,0 +1,17 @@
+package pl.edu.utp.medicalassistant.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Data
+public class Rescuer {
+
+    private String rescuerId;
+    private Date date;
+    private Location location;
+    private String status;
+
+}
