b2587b9b409142300230f4d67b8afcc6360bd4b2
Damian <damian.ledzinski@utp.edu.pl>
2018-04-14 11:48:11 +0200

    information mock

--- src/main/java/pl/edu/utp/medicalassistant/controller/InformationController.java
+++ src/main/java/pl/edu/utp/medicalassistant/controller/InformationController.java
@@ -2,12 +2,15 @@ package pl.edu.utp.medicalassistant.controller;
 
 import io.swagger.annotations.Api;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.utp.medicalassistant.model.information.Information;
+import pl.edu.utp.medicalassistant.model.information.InformationType;
 import pl.edu.utp.medicalassistant.service.InformationService;
 
 @RestController
@@ -20,7 +23,26 @@ public class InformationController {
 	private InformationService informationService;
 	
     @GetMapping("/all")
-    public List<Information> getInformations(){
-        return informationService.getInformations();
+    public List<Information> getInformations(
+			@RequestParam(value = "danger-live", required = false, defaultValue = "true") Boolean dangerLive,
+			@RequestParam(value = "danger-health", required = false, defaultValue = "true") Boolean dangerHealth,
+			@RequestParam(value = "help", required = false, defaultValue = "true") Boolean help,
+			@RequestParam(value = "other", required = false, defaultValue = "true") Boolean other,
+			@RequestParam(value = "rescuer", required = false, defaultValue = "true") Boolean rescuer,
+			@RequestParam(value = "medical-rescuer", required = false, defaultValue = "true") Boolean medicalRescuer,
+			@RequestParam(value = "other-person", required = false, defaultValue = "true") Boolean otherPerson){
+		
+        return informationService.getInformations()
+				.stream()
+				.filter(i -> i.getInformationType() != InformationType.DANGER_HEALTH || dangerLive)
+				.filter(i -> i.getInformationType() != InformationType.DANGER_LIFE || dangerHealth)
+				.filter(i -> i.getInformationType() != InformationType.HELP || help)
+				.filter(i -> i.getInformationType() != InformationType.OTHER || other)
+				.filter(i -> i.getInformationType() != InformationType.RESCUER || rescuer)
+				.filter(i -> i.getInformationType() != InformationType.MEDICAL_RESCUER || medicalRescuer)
+				.filter(i -> i.getInformationType() != InformationType.OTHER_PERSON || otherPerson)
+				.collect(Collectors.toList());
     }
+	
+	
 }
